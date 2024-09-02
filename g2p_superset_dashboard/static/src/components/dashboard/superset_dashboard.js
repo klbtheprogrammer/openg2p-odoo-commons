aff6198238a8e6af8826c1993b1d3a74415a0faa
EyuaelB <eyuaelbezabeh@gmail.com>
2024-09-02 10:02:17 +0300

    removed login function

--- g2p_superset_dashboard/static/src/components/dashboard/superset_dashboard.js
+++ g2p_superset_dashboard/static/src/components/dashboard/superset_dashboard.js
@@ -36,38 +36,6 @@ export class G2PSupersetDashboard extends Component {
 
         this.state.isLoading = false;
     }
-
-    async attemptLogin() {
-        try {
-            console.log(`this is the url ${this.superSetUrl}/api/v1/security/login`);
-
-            const response = await fetch(`${this.superSetUrl}/api/v1/security/login`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    password: "tr17",
-                    provider: "db",
-                    refresh: true,
-                    username: "tr17",
-                }),
-                credentials: "include",
-            });
-
-            if (!response.ok) {
-                throw new Error(`Login failed with status ${response.status}`);
-            }
-
-            console.log(`The login API request is successful here is status ${response.status}`);
-
-            const data = await response.json();
-            return {success: data.success};
-        } catch (error) {
-            console.error("Error during login:", error);
-            return {success: false};
-        }
-    }
 }
 
 G2PSupersetDashboard.template = "g2p_superset_dashboard.G2PSupersetDashboard";
